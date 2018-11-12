3bc97e487fd6c13afd4974e8318b8f589125a0d1
shaplahost <hmbashar@gmail.com>
2018-11-12 15:22:40 +0600

    Add URL Validation Check

--- functions/validate.php
+++ functions/validate.php
@@ -53,3 +53,22 @@ if( ! function_exists( 'cs_validate_required' ) ) {
   }
   add_filter( 'cs_validate_required', 'cs_validate_required' );
 }
+
+/**
+ *
+ * URL validate
+ *
+ * @since 1.0.0
+ * @version 1.0.0
+ *
+ */
+if( ! function_exists( 'cs_validate_url' ) ) {
+  function cs_validate_url( $value, $field ) {
+    if (!empty($value)) {
+        if ( ! filter_var($value, FILTER_VALIDATE_URL) ) {
+            return esc_html__( 'Please write a valid url!', 'cs-framework' );
+          }
+    }
+  }
+  add_filter( 'cs_validate_url', 'cs_validate_url', 10, 2 );
+}
